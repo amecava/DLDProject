de17b27550b207a6972bbaff91c1ed324ea4481f
Amedeo Cavallo <amedeo.cavallo96@gmail.com>
2019-03-01 17:49:21 +0100

    Update run.py

--- xelab/run.py
+++ xelab/run.py
@@ -6,11 +6,6 @@ import argparse
 import subprocess
 from string import Template
 
-# Set settings64 paths for Vivado setup
-settings64Vivado = r"CALL C:\Xilinx\Vivado\2018.3\.settings64-Vivado.bat & "
-settings64DocNav = r"CALL C:\Xilinx\DocNav\.settings64-DocNav.bat & "
-settings64SDKCoreTools = r"CALL C:\Xilinx\SDK\2018.3\.settings64-SDK_Core_Tools.bat & "
-
 def to_binary(num):
     # Convert integer to binary array
     return list(map(int, list("".join(str(1 & int(num) >> i) for i in range(8)[::-1]))))
@@ -23,15 +18,15 @@ def to_int(bin_array):
     
     return num
 
-def compute_result(value):
+def compute_result(tb_values):
     # Input bitmask and set mindist
-    bitmask = to_binary(value[0])
+    bitmask = to_binary(tb_values[0])
     mindist = 511
 
     # Calculate Manhattan distance
     for i in range(0, 8):
         if bitmask[i] == 1:
-            distance = abs(value[9][0] - value[8 - i][0]) + abs(value[9][1] - value[8 - i][1])
+            distance = abs(tb_values[9][0] - tb_values[8 - i][0]) + abs(tb_values[9][1] - tb_values[8 - i][1])
 
             if distance < mindist:
                 for j in range(0, i):
@@ -45,39 +40,52 @@ def compute_result(value):
     return to_int(bitmask)
 
 def generate_input(mode):
-    value = []
+    tb_values = []
 
     # Input mode selection
     if mode == 's':
         # Stdin bitmask and centroids
-        value.append(int(input("Bitmask: ")))
+        tb_values.append(int(input("Bitmask: ")))
         for i in range(1, 9):
             x, y = input("Centroid " + str(i) + " X Y: ").split()
-            value.append([int(x), int(y)])
+            tb_values.append([int(x), int(y)])
         x, y = input("Point X Y: ").split()
-        value.append([int(x), int(y)])
+        tb_values.append([int(x), int(y)])
 
     elif mode == 'r':
         # Random bitmask and random centroids
-        value.append(random.randint(0, 255))
+        tb_values.append(random.randint(0, 255))
         for _ in range(1, 10):
-            value.append([random.randint(0, 255), random.randint(0, 255)])
+            tb_values.append([random.randint(0, 255), random.randint(0, 255)])
 
     # Create template dictionary
-    d = {'BITMASK': value[0], 
-         'XC1': value[1][0], 'YC1': value[1][1],
-         'XC2': value[2][0], 'YC2': value[2][1],
-         'XC3': value[3][0], 'YC3': value[3][1],
-         'XC4': value[4][0], 'YC4': value[4][1],
-         'XC5': value[5][0], 'YC5': value[5][1],
-         'XC6': value[6][0], 'YC6': value[6][1],
-         'XC7': value[7][0], 'YC7': value[7][1],
-         'XC8': value[8][0], 'YC8': value[8][1],
-         'X': value[9][0], 'Y': value[9][1],
-         'RESULT': compute_result(value)
+    d = {'BITMASK': tb_values[0], 
+         'XC1': tb_values[1][0], 'YC1': tb_values[1][1],
+         'XC2': tb_values[2][0], 'YC2': tb_values[2][1],
+         'XC3': tb_values[3][0], 'YC3': tb_values[3][1],
+         'XC4': tb_values[4][0], 'YC4': tb_values[4][1],
+         'XC5': tb_values[5][0], 'YC5': tb_values[5][1],
+         'XC6': tb_values[6][0], 'YC6': tb_values[6][1],
+         'XC7': tb_values[7][0], 'YC7': tb_values[7][1],
+         'XC8': tb_values[8][0], 'YC8': tb_values[8][1],
+         'X': tb_values[9][0], 'Y': tb_values[9][1],
+         'RESULT': compute_result(tb_values)
         }
 
     return d
+
+def vivado_settings_path():
+    # Search settings64 bat files
+    for root, _, files in os.walk(r"C:\\"):
+        for name in files:
+            if name == ".settings64-Vivado.bat":
+                settings64Vivado = os.path.abspath(os.path.join(root, name))
+            if name == ".settings64-DocNav.bat":
+                settings64DocNav = os.path.abspath(os.path.join(root, name))
+            if name == ".settings64-SDK_Core_Tools.bat":
+                settings64SDKCoreTools = os.path.abspath(os.path.join(root, name))
+
+    return settings64Vivado, settings64DocNav, settings64SDKCoreTools
 
 def main():
     # Argument parser
@@ -85,6 +93,21 @@ def main():
     parser.add_argument("-n", action="store", dest="n", type=int, default=1, help="NUMBER of simulations [default = 1].")
     parser.add_argument('-m', choices=['s', 'r'], default='r', help="input mode selection: s for STDIN / r for RANDOM [default = r].")
     args = parser.parse_args()
+
+    # Find settings64 paths
+    settings_path = ['', '', '']
+    sys.stdout.write(r"Searching settings64 bat files in C:\ ")
+    sys.stdout.flush()
+    settings_path[0], settings_path[1], settings_path[2] = vivado_settings_path()
+    print("\n=> " + settings_path[0])
+    print("=> " + settings_path[1])
+    print("=> " + settings_path[2])
+    
+    # Create settings64 calls
+    settings_call = ("CALL " + settings_path[0] + " & " +
+                     "CALL " + settings_path[1] + " & " +
+                     "CALL " + settings_path[2] + " & "
+                    )
 
     # Testbench template file
     filein = open("tb_template.vhd")
@@ -98,18 +121,16 @@ def main():
         print("\nSimulation ID: " + str(i + 1))
 
         # Generate input and create testbench
-        value = generate_input(args.m)
-        fileout.write(template.substitute(value))
+        tb_values = generate_input(args.m)
+        fileout.write(template.substitute(tb_values))
         fileout.close()
 
-        print("    Expected return value: " + str(bin(value.get('RESULT'))))
+        print("    Expected return value: " + str(bin(tb_values.get('RESULT'))))
         sys.stdout.write("    Running simulation ")
         sys.stdout.flush()
 
         # Run behavioural simulation
-        bash = subprocess.getoutput(settings64Vivado +
-                                    settings64DocNav +
-                                    settings64SDKCoreTools +
+        bash = subprocess.getoutput(settings_call +
                                     "xvhdl temporary.vhd & " +
                                     "xvhdl project_reti_logiche.vhd & " +
                                     "xelab project_tb & " +
@@ -125,14 +146,12 @@ def main():
             
             # GUI waveform debugging
             if input("=> Simulation failed, run GUI waveform debugging? [y] ").lower() == 'y':
-                bash = subprocess.getoutput(settings64Vivado +
-                                    settings64DocNav +
-                                    settings64SDKCoreTools +
-                                    "xvhdl temporary.vhd & " +
-                                    "xvhdl project_reti_logiche.vhd & " +
-                                    "xelab project_tb -debug all & " +
-                                    "xsim work.project_tb -gui"
-                                    )
+                bash = subprocess.getoutput(settings_call +
+                                            "xvhdl temporary.vhd & " +
+                                            "xvhdl project_reti_logiche.vhd & " +
+                                            "xelab project_tb -debug all & " +
+                                            "xsim work.project_tb -gui"
+                                            )
 
     # Remove temporary testbench file
     if os.path.exists("temporary.vhd"):
